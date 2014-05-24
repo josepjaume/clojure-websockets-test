1d11b35b70e5801d00a43db82462b221224fb66a
Josep Jaume Rey <josepjaume@gmail.com>
2014-05-24 13:15:50 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hiya
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ src/hiya/core.clj
@@ -0,0 +1,39 @@
+(ns hiya.core
+  (:use [compojure.core :only (defroutes GET)]
+        ring.util.response
+        ring.middleware.cors
+        org.httpkit.server)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [ring.middleware.reload :as reload]
+            [cheshire.core :refer :all]))
+
+(def clients (atom {}))
+
+(defn ws [req]
+  (with-channel req channel
+    (swap! clients assoc channel (str (java.util.UUID/randomUUID)))
+    (println channel " connected")
+    (on-close channel (fn [status]
+                        (swap! clients dissoc channel)
+                        (println channel " disconnected. status: " status)))
+    (on-receive channel (fn [data]
+                          (doseq [[client id] (dissoc @clients channel)]
+                            (send! client
+                                   (generate-string {:id id :data (parse-string data)}))
+                            )))))
+
+(defroutes routes
+  (GET "/socket" [] ws)
+  (GET "/" [] (file-response "index.html")))
+
+
+(def application (-> (handler/site routes)
+                     reload/wrap-reload
+                     (wrap-cors
+                      :access-control-allow-origin #".+")))
+
+(defn -main [& args]
+  (let [port (Integer/parseInt
+               (or (System/getenv "PORT") "8080"))]
+    (run-server application {:port port :join? false})))
